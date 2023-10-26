ad799802b6e5fbdd98c3b39a4e7279073d0eee98
katherineclark-webster <katherine.c@thoughtworks.com>
2023-10-26 16:31:33 +0100

    signup can create a new user and linked to homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @user = User.new()
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User was created successfully."
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
